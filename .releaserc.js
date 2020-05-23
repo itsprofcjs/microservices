6e12a36d48a25221b08f111674abebbf2f058282
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-23 10:41:42 +0530

    Initial commit

---
+++ .releaserc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    ci: true,
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
+        [
+            '@semantic-release/npm',
+            {
+                npmPublish: false,
+                tarballDir: 'dist',
+            },
+        ],
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+            },
+        ],
+    ],
+};
