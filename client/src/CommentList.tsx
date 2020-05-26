d33ae2b2a97991783c8c82395a9cca4092f86d4c
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-05-26 18:05:01 +0530

    feat(blog): Blog now available

--- client/src/CommentList.tsx
+++ client/src/CommentList.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 function CommentList({ comments }: any) {
     const renderedComments = () =>
-        comments.map((comment: any) => (
-            <li key={comment.id}>{comment.content}</li>
-        ));
+        comments.map((comment: any) => {
+            let content = comment.content;
+
+            if (comment.status === 'pending') {
+                content = 'Comment is awaiting approval';
+            } else if (comment.status === 'rejected') {
+                content = 'Comment is rejected';
+            } else if (comment.status === 'pending') {
+                content = 'Comment is awaiting moderation';
+            }
+
+            return <li key={comment.id}>{content}</li>;
+        });
 
     return (
         <article className="content">
